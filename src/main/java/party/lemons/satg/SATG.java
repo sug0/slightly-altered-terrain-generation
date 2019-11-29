a0c1479096d86049ef2e6a27ddc72249479a8ced
Sam <lemonverse@gmail.com>
2019-11-30 10:01:42 +1300

    1.14

---
+++ src/main/java/party/lemons/satg/SATG.java
@@ -0,0 +1,12 @@
+package party.lemons.satg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SATG implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("i love u");
+	}
+}
---
+++ src/main/java/party/lemons/satg/mixins/OverworldChunkGeneratorMixin.java
@@ -0,0 +1,114 @@
+package party.lemons.satg.mixins;
+
+import net.minecraft.util.math.noise.SimplexNoiseSampler;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.gen.chunk.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(OverworldChunkGenerator.class)
+public abstract class OverworldChunkGeneratorMixin extends SurfaceChunkGenerator<OverworldChunkGeneratorConfig>
+{
+	@Shadow public abstract int getSeaLevel();
+	@Shadow public abstract double sampleNoise(int x, int y);
+
+	@Shadow @Final private boolean amplified;
+	@Shadow @Final private static float[] BIOME_WEIGHT_TABLE;
+
+	private SimplexNoiseSampler simplexSampler;
+
+	public OverworldChunkGeneratorMixin(IWorld world, BiomeSource biomeSource, int verticalNoiseResolution, int horizontalNoiseResolution, int worldHeight, OverworldChunkGeneratorConfig config, boolean useSimplexNoise)
+	{
+		super(world, biomeSource, verticalNoiseResolution, horizontalNoiseResolution, worldHeight, config, useSimplexNoise);
+	}
+
+
+	@Overwrite
+	public double[] computeNoiseRange(int x, int z)
+	{
+		if(simplexSampler == null)
+		{
+			this.random.consume(2620);
+			this.simplexSampler = new SimplexNoiseSampler(this.random);
+		}
+
+		double[] noiseRange = new double[2];
+		float f = 0.0F;
+		float g = 0.0F;
+		float h = 0.0F;
+		int seaLevel = this.getSeaLevel();
+
+		Biome baseBiome = this.biomeSource.getBiomeForNoiseGen(x, z);
+		float baseDepth = baseBiome.getDepth();
+		if(!keepBiomeProportions(baseBiome))
+			baseDepth /= 2;
+
+		for(int xOffset = -2; xOffset <= 2; ++xOffset) {
+			for(int zOffset = -2; zOffset <= 2; ++zOffset) {
+				Biome biome = this.biomeSource.getBiomeForNoiseGen(x + xOffset, z + zOffset);
+
+				float originalDepth = keepBiomeProportions(biome) ? biome.getDepth() : getAdjustedDepth(biome, x, z, xOffset, zOffset);
+				float depth = originalDepth;
+				float scale = keepBiomeProportions(biome) ? biome.getScale() : getAdjustedScale(biome, x, z, xOffset, zOffset);
+
+				if (this.amplified && depth > 0.0F) {
+					depth = 1.0F + depth * 2.0F;
+					scale = 1.0F + scale * 4.0F;
+				}
+
+				float p = BIOME_WEIGHT_TABLE[xOffset + 2 + (zOffset + 2) * 5] / (depth + 2.0F);
+				if (originalDepth > baseDepth)
+				{
+					p /= 2.0F;
+				}
+
+				f += scale * p;
+				g += depth * p;
+				h += p;
+			}
+		}
+
+		f /= h;
+		g /= h;
+		f = f * 0.9F + 0.1F;
+		g = (g * 4.0F - 1.0F) / 8.0F;
+		noiseRange[0] = (double)g + this.sampleNoise(x, z);
+		noiseRange[1] = f;
+		return noiseRange;
+	}
+
+	public float getAdjustedDepth(Biome biome, int x, int z, int xOffset, int zOffset)
+	{
+		if(keepBiomeProportions(biome))
+			return biome.getDepth();
+
+		Biome stealBiome = this.biomeSource.getBiomeForNoiseGen(x + xOffset + 99999, z + zOffset + 99999);
+		return keepBiomeProportions(stealBiome) ? biome.getDepth() : stealBiome.getDepth();
+	}
+
+	public float getAdjustedScale(Biome biome, int x, int z, int xOffset, int zOffset)
+	{
+		if(keepBiomeProportions(biome))
+			return biome.getDepth();
+
+		Biome stealBiome = this.biomeSource.getBiomeForNoiseGen(x + xOffset + 99999, z + zOffset + 99999);
+		if(keepBiomeProportions(stealBiome))
+			return biome.getScale();
+		else
+		{
+			float adjustedScale = (float) Math.abs(sampleNoise(x + xOffset, z + zOffset) * 15F);
+			return stealBiome.getScale() + adjustedScale;
+		}
+	}
+
+	public boolean keepBiomeProportions(Biome biome)
+	{
+		Biome.Category category = biome.getCategory();
+		return category == Biome.Category.OCEAN || category == Biome.Category.RIVER || category == Biome.Category.BEACH;
+	}
+}
+
